bf7abea39135983b6d6bf4414fd855c9c59effac
inclyc <me@inclyc.cn>
2022-06-25 13:48:02 +0800

    mem: add comment and level size generator

--- src/mem/mod.rs
+++ src/mem/mod.rs
@@ -20,6 +20,7 @@ impl<K: Eq + Hash, V> TimestampElement<K, V> {
 struct MemoryBuffer<K, V> {
     levels: Vec<Option<VecDeque<TimestampElement<K, V>>>>,
     lvlsize: usize,
+    level_limit_generator: Option<fn(usize) -> usize>,
 }
 
 struct QueryResult<V> {
@@ -27,10 +28,17 @@ struct QueryResult<V> {
 }
 
 impl<K: Eq + Hash, V> MemoryBuffer<K, V> {
-    fn new(lvlsize: usize) -> MemoryBuffer<K, V> {
+    fn new(
+        lvlsize: usize,
+        level_limit_generator: Option<fn(usize) -> usize>,
+    ) -> MemoryBuffer<K, V> {
         let mut levels = Vec::with_capacity(lvlsize);
         levels.resize_with(lvlsize, || None);
-        MemoryBuffer { levels, lvlsize }
+        MemoryBuffer {
+            levels,
+            lvlsize,
+            level_limit_generator,
+        }
     }
 
     fn _write_level(te: &mut VecDeque<TimestampElement<K, V>>, timestamp: u64, pk: K, value: V) {
@@ -39,7 +47,7 @@ impl<K: Eq + Hash, V> MemoryBuffer<K, V> {
                 if data.timestamp == timestamp {
                     data.dict.insert(pk, value);
                 } else {
-                    // this is a new timestamp here
+                    // 这是一个新的时间戳
                     te.push_back(TimestampElement::new(timestamp, pk, value))
                 }
             }
@@ -47,12 +55,20 @@ impl<K: Eq + Hash, V> MemoryBuffer<K, V> {
         }
     }
 
+    /// 写入到这个时序数据库
+    /// Write to this TSDB
     fn write(&mut self, timestamp: u64, pk: K, value: V) {
-        let lvl = timestamp.trailing_zeros() as usize;
+        let lvl = timestamp.trailing_zeros() as usize; // 时间戳级别，定义为时间戳末尾0的数量
+
+        // 这个级别所对应的双端队列，队列可能不存在，levels这个Vec在初始化时全部用None填充了
         let level = self.levels.get_mut(lvl).unwrap();
         match level {
             None => {
-                let mut te = VecDeque::new();
+                // 这个级别以前从未达到过，需要新创建一个level
+                let mut te = match self.level_limit_generator {
+                    Some(g) => VecDeque::with_capacity(g(lvl)),
+                    None => VecDeque::new(),
+                };
                 MemoryBuffer::_write_level(&mut te, timestamp, pk, value);
                 *level = Some(te);
             }
@@ -60,6 +76,9 @@ impl<K: Eq + Hash, V> MemoryBuffer<K, V> {
         };
     }
 
+    /// 按照主键(pk)和层级(level)查询内存区数据
+    /// Query in-memory buffer data by Primary Key(pk) and Level(level)
+    /// 返回查询结果
     fn query(&self, pk: K, level: usize) -> QueryResult<&V> {
         let mut result = BTreeSet::new();
         for idx in level..self.lvlsize {
@@ -87,7 +106,7 @@ mod test {
 
     #[test]
     fn test_write_query() {
-        let mut buffer = MemoryBuffer::new(64);
+        let mut buffer = MemoryBuffer::new(64, None);
         let test_pk = "some_pk";
         for i in 1..8 {
             buffer.write(i, test_pk, i);

89ec3234473c98295cc76ec22b4bffe2a112d881
YingChi Long <me@inclyc.cn>
2022-09-06 20:13:30 +0800

    [scripts] update lru to use latex

--- scripts/lru_size.py
+++ scripts/lru_size.py
@@ -4,6 +4,9 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 
+plt.rcParams['text.usetex'] = True
+
+
 def main():
     all_data = []
     for line in stdin:
@@ -11,7 +14,8 @@ def main():
         all_data.append(level_cache_miss[:3])
     cache_index = np.arange(len(all_data))
     plt.plot(cache_index, all_data, label=['LV0', 'LV1', 'LV2'])
-    plt.xlabel("Cache size")
+    plt.title("Cache misses by queries subject to normal distribution")
+    plt.xlabel(r"$\sigma$")
     plt.ylabel("Cache misses")
     plt.legend()
     plt.show()
